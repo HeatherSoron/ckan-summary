2faa146f81d443ebb6b50f16a9105b34c47c640f
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2015-03-12 08:35:35 +1300

    Switch to parsing Git history to find CKAN files to read

--- main.rb
+++ main.rb
@@ -2,14 +2,21 @@ require 'json'
 
 $main_info = {};
 
-proj_list = Dir['/home/ethan/games/kerbal/CKAN-meta/*']
+ckan_dir = '/home/ethan/games/kerbal/CKAN-meta'
 
-proj_list.each do |proj|
-	versions = Dir["#{proj}/*.ckan"]
-	versions.each do |v|
-		puts "Reading #{v}"
-		info = JSON.parse(File.read(v))
+proj_list = Dir['#{ckan_dir}/*']
+
+# sweet! "split" on a string handles blank lines in exactly the way I want :)
+files = `cd #{ckan_dir}; git log --name-only --format='format:' HEAD~5..HEAD`.split
+
+files.each do |f|
+	path = "#{ckan_dir}/#{f}"
+	if File.exists?(path) then
+		puts "Reading #{f}"
+		info = JSON.parse(File.read(path))
 		$main_info[info['identifier']] = info
+	else
+		puts "No such file #{f}"
 	end
 end
 
