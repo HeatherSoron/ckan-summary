8f7c35a634698bd6a2b49a641abd2aafd5eccb91
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2015-03-11 18:35:11 +1300

    Format output a bit better

--- main.rb
+++ main.rb
@@ -4,13 +4,18 @@ $main_info = {};
 
 proj_list = Dir['/home/ethan/games/kerbal/CKAN-meta/*']
 
-proj_list.each { |proj|
+proj_list.each do |proj|
 	versions = Dir["#{proj}/*.ckan"]
-	versions.each { |v|
+	versions.each do |v|
 		puts "Reading #{v}"
 		info = JSON.parse(File.read(v))
-		$main_info[info['identifier']] = info['version']
-	}
-}
+		$main_info[info['identifier']] = info
+	end
+end
 
-puts $main_info
+puts "Done reading\n\n"
+
+$main_info.keys.sort.each do |key|
+	info = $main_info[key]
+	puts "#{info['name']}: #{info['abstract']}"
+end
