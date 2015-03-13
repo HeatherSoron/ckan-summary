066d225bb03b6cf6d5306124715820d1230387cb
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2015-03-13 18:42:36 +1300

    Add in timestamp-based restore points to CKAN-meta parser
    Maybe I should use something that equates to a "$commit > $last", though, instead of "$commit >= $last"...

--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
 require 'json'
 
+# side note: coding in an unfamiliar language on the train (no wifi) is a REALLY interesting experience.
+# I feel sorta like I'm back in the days when you'd learn a language with an actual book, or manpages, or something,
+# as opposed to the modern approach of just googling trivial questions and ending up on StackOverflow.
+
 $main_info = {};
 
 settings_dir = "#{ENV['HOME']}/.soron-tools/ckan-info"
@@ -14,11 +18,22 @@ if !settings['ckan-dir'] then
 	settings['ckan-dir'] = readline.split[0]
 end
 
+if !settings['last-check'] then
+	# default to unix epoch, explicitly. I'm PRETTY sure that'll predate all CKAN-meta commits by a good 40+ years.
+	settings['last-check'] = 0
+end
+
 proj_list = Dir["#{settings['ckan-dir']}/*"]
 
-# sweet! "split" on a string handles blank lines in exactly the way I want :)
-files = `cd #{settings['ckan-dir']}; git log --name-only --format='format:' HEAD~5..HEAD`.split
+# we'll be using this basic command twice with a few different args, let's just collect the start of it into a variable
+git_log_preamble = "cd #{settings['ckan-dir']}; git log --since=\"$(date -d @#{settings['last-check']})\""
 
+# sweet! "split" on a string handles blank lines in exactly the way I want :)
+files = `#{git_log_preamble} --name-only --format='format:'`.split
+
+last_date = `#{git_log_preamble} --format='%ct'`.split.max
+
+# TODO filter out non-JSON files
 files.each do |f|
 	path = "#{settings['ckan-dir']}/#{f}"
 	if File.exists?(path) then
@@ -35,6 +50,12 @@ puts "Done reading\n\n"
 $main_info.keys.sort.each do |key|
 	info = $main_info[key]
 	puts "#{info['name']}: #{info['abstract']}"
+end
+
+puts "\nLast timestamp was #{last_date}. Do you want to save that as the starting point for future searches? (y/n)"
+# there are probably better ways of handling a y/n prompt. Actually, maybe even a built-in library?
+if readline == "y\n" then
+	settings['last-check'] = last_date
 end
 
 # TODO handle mkdir
